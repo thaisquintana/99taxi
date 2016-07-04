2d65e1f26e8e68c88bedcb7891bc701e094a514d
thaisquintana <thaisquintanap@gmail.com>
2016-07-04 00:44:07 -0300

    mascara telefone

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("mask()",1)
+}
+function mask(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function phonumber(v){
+    v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
+    return v;
+}
+function id( el ){
+	return document.getElementById( el );
+}
+window.onload = function(){
+	id('phonefield').onkeyup = function(){
+		mascara( this, phonumber );
+	}
+}
 // Envio do formulario
 $(document).ready(function() {
     $('#contactform').on('submit', function(e) {
